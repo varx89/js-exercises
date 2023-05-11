6073743775809da586947c252478cec874606d6c
Andrei Varcus <andrei.varcus@yahoo.com>
2023-05-11 21:07:19 +0300

    finished exercise 2

---
+++ homework session 16/hw2.js
@@ -0,0 +1,36 @@
+/*
+2. Creeaza o funcție care gaseste toate valorile duplicate intr-un array
+ și returnează o lista cu toate valorile care apar de mai multe ori.
+*/
+
+let numbers = [8, 19, 65, 33, 8, 22, 19, 33, 4, 16, 23, 22, 33];
+
+const showDuplicates = (myArray) => {
+  let array = [...myArray];
+  let resArray = [];
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array.length; j++) {
+      if (array[i] === array[i + j + 1] && resArray.indexOf(array[i]) < 0) {
+        resArray.push(array[i]);
+      }
+    }
+  }
+  return resArray;
+};
+
+console.log(showDuplicates(numbers));
+
+//return without duplicates
+const notShowDuplicates = (myArray) => {
+  let array = [...myArray];
+  let resArray = [];
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array.length; j++) {
+      if (array[i] !== array[i + j + 1] && resArray.indexOf(array[i]) < 0) {
+        resArray.push(array[i]);
+      }
+    }
+  }
+  return resArray;
+};
+console.log(notShowDuplicates(numbers));
