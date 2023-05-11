7dc8d0b34bd98261a60990141ee6d5d704136a88
Andrei Varcus <andrei.varcus@yahoo.com>
2023-05-11 23:56:10 +0300

    finished exercises 3-4-5

---
+++ homework session 16/hw3-4-5.js
@@ -0,0 +1,68 @@
+/*
+3. Creeaza o funcție care gaseste cel mai lung 
+string intr-un array și returnează acest string.
+*/
+
+let stringArray = ["Andrei", "Ada", "Diana", "Alexandra", "Anastasia"];
+
+const maxStringLength = (myArray) => {
+  let highString = myArray[0];
+  for (let i = 0; i < myArray.length; i++) {
+    if (myArray[i].length > highString.length) {
+      highString = myArray[i];
+    }
+  }
+  return highString;
+};
+
+console.log(maxStringLength(stringArray));
+
+/*
+4. Creeaza o funcție care face conversia 
+numerelor romane (pana la 1000) in numere intregi.
+*/
+
+const convertIntRomanize = (untilNum) => {
+  for (let i = 1; i <= untilNum; i++) {
+    console.log(romanize(i));
+  }
+  function romanize(num) {
+    const romanLetters = {
+      M: 1000,
+      CM: 900,
+      D: 500,
+      CD: 400,
+      C: 100,
+      XC: 90,
+      L: 50,
+      XL: 40,
+      X: 10,
+      IX: 9,
+      V: 5,
+      IV: 4,
+      I: 1,
+    };
+    let res = "";
+
+    for (let key in romanLetters) {
+      while (num >= romanLetters[key]) {
+        res += key;
+        num -= romanLetters[key];
+      }
+    }
+    return res;
+  }
+};
+
+convertIntRomanize(10);
+
+/*
+5. Creeaza o funcție care face conversia 
+gradelor Celsius in grade Fahrenheit.
+*/
+
+const celsiusToFahrenheit = (grade) => {
+  return (grade * 9) / 5 + 32;
+};
+
+console.log(celsiusToFahrenheit(40));
