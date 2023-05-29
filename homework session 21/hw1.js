9e11a202150244ee14f22ed19feeece3d5c09278
Andrei Varcus <andrei.varcus@yahoo.com>
2023-05-29 20:22:42 +0300

    starting homework session 21

---
+++ homework session 21/hw1.js
@@ -0,0 +1,23 @@
+/*
+Creeaza o clasa care reprezinta o banca si are urmatoarele proprietati:
+  • Nume
+  • Cod SWIFT
+  • Tara de origine
+  • Lista de conturi deschise
+  • Metoda pentru crearea unui cont
+  • Metoda pentru afisarea conturilor deschise
+  • Metoda pentru afisarea conturilor cu sold negativ
+
+*/
+
+/*
+Folosind constructor functions, creeaza modelul unui cont bancar pe care îl poți adăuga 
+  in banca realizata mai sus. Acestea au următoarele proprietăți:
+    • Numar cont
+    • ID
+    • Sold
+    • Nume detinator cont
+Folosind prototype, adaugă o metoda pe constructor function-ul de mai sus, cu ajutorul 
+  căruia sa poți achita o suma X din cont.
+
+*/
