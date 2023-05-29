cc6c2a0c6e9bfbf773bed4f7b87a4a49bfa7810d
Andrei Varcus <andrei.varcus@yahoo.com>
2023-05-29 20:46:03 +0300

    finished class and constructor function as base

--- homework session 21/hw1.js
+++ homework session 21/hw1.js
@@ -10,6 +10,28 @@ Creeaza o clasa care reprezinta o banca si are urmatoarele proprietati:
 
 */
 
+class Bank {
+  name;
+  swift;
+  country;
+  // listAccounts;
+  // createAccounts;
+  // negativeAccounts;
+
+  constructor(name, swift, country) {
+    this.name = name;
+    this.swift = swift;
+    this.country = country;
+  }
+
+  listAccounts() {}
+  createAccounts() {}
+  negativeAccounts() {}
+}
+
+const bank1 = new Bank('Deutsche Bank', 'RZRPROBU', 'Romania');
+console.log(bank1);
+
 /*
 Folosind constructor functions, creeaza modelul unui cont bancar pe care îl poți adăuga 
   in banca realizata mai sus. Acestea au următoarele proprietăți:
@@ -21,3 +43,19 @@ Folosind prototype, adaugă o metoda pe constructor function-ul de mai sus, cu a
   căruia sa poți achita o suma X din cont.
 
 */
+
+function BankAccount(accountNumber, ID, balance, accountName) {
+  this.accountNumber = accountNumber;
+  this.ID = ID;
+  this.balance = balance;
+  this.fullName = accountName;
+}
+
+const acc1 = new BankAccount(
+  'RORNCB00012320909796',
+  Date.now(),
+  19233,
+  'Andrei Varcus'
+);
+
+console.log(acc1);
