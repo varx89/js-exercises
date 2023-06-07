2528fbc9b9345ce47b1b86360fbcbbba453ddacd
Andrei Varcus <andrei.varcus@yahoo.com>
2023-06-07 20:23:29 +0300

    started project calculator

---
+++ homework session 22/calculator.js
@@ -0,0 +1,9 @@
+/*
+Folosind cele invatate, poți face interfata unui calculator, respectiv functionalitatea lui.
+
+Valideaza input-ul, astfel incat utilizatorul sa nu poată introduce altceva inafara de numere.
+
+Tot pe aceeași pagina afișează și rezultatul calculului.
+
+
+*/
