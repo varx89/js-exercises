0b133df9ec75d04d4aeb9a9b349dad19b73764d3
Andrei Varcus <andrei.varcus@yahoo.com>
2023-06-13 00:58:15 +0300

    styling project

---
+++ homework session 23/fetchApi.js
@@ -0,0 +1,15 @@
+/*
+Folosind Random Data API, preia o lista de 10 utilizatori și afișează pe ecran pe toți. Design-ul este la alegerea ta.
+
+Include cel puțin următoarele informații:
+Nume intreg
+Email
+Username
+Avatar
+Country
+City
+Employment Title
+
+
+*/
+const url = 'https://random-data-api.com/api/v2/users?size=10';
